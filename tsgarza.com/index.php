c610b3b5afcd209cb46ff767775a3f3f098b9716
alancarba <shawn666_00@hotmail.com>
2018-03-22 02:01:09 -0500

    Add files via upload

--- tsgarza.com/index.php
+++ tsgarza.com/index.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<title>TS & Garza</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,500,900" rel="stylesheet">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/style.css">
 </head>
@@ -28,7 +29,24 @@
 				</ul>
 			</nav>
 		</div>
+		<div id="slider"></div>
 	</header>
+	<div class="clearfix"></div>
+	<section id="contenedorNosotros">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-9">
+					<h2>Nosotros</h2>
+					<h1>CREAMOS<br/>SOLUCIONES</h1>
+					<h3>a tus necesidades</h3>
+					<p>El éxito de un negocio es dar un excelente servicio o producto a todos nuestros clientes. Toda empresa con éxito va de la mano con una <b>Asesoría Contable, Fiscal y Financiera</b>; es por eso sometemos a su consideración nuestra propuesta de servicios proponiendo el mejor esquema diseñado para atender sus necesidades, buscando rentabilidad, legalidad y profesionalismo.</p>
+					<p>Nuestro equipo de trabajo reúne el conocimiento y la experiencia de varios años de trabajo en diversas empresas que nos han permitido unir esfuerzos y desarrollar los esquemas de control contable, fiscal y legal que harán más eficiente a su empresa.
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="topHeaderServicios"></section>
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
